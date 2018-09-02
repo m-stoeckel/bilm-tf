1aae70aebbf4fa0ca48584a34bfabcf9497464af
Manuel Stoeckel <s3676959@stud.uni-frankfurt.de>
2018-09-02 15:35:19 +0200

    run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-python3 bin/train_elmo_n_gpus.py --train_prefix '/ssd4/public/stoeckel/data/Leipzig40MT2010_lowered/Leipzig40MT2010_lowered_training/*' \
---save_dir '/ssd4/public/stoeckel/elmo/Leipzig40MT2010_lowered' --vocab_file '/ssd4/public/stoeckel/data/Leipzig40MT2010_lowered/Leipzig40MT2010_lowered.5.vocab' \
---n_tokens 1098163591 --use_gpus 2 --batchsize 256
+export CUDA_VISIBLE_DEVICES=0,1
+python3 bin/train_elmo_n_gpus.py --train_prefix '/ssd4/public/stoeckel/data/Leipzig40MT2010_lowered/Leipzig40MT2010_lowered_training/*' --save_dir '/ssd4/public/stoeckel/elmo/Leipzig40MT2010_lowered' --vocab_file '/ssd4/public/stoeckel/data/Leipzig40MT2010_lowered/Leipzig40MT2010_lowered.5.vocab' --n_tokens 1098163591 --use_gpus 2 --batchsize 512
